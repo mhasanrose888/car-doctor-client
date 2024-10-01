1c79c0b5f9b502e570dbb93709b047879ab00a6c
mhasanrose888 <mhasanrose888@gmail.com>
2024-10-01 06:13:09 +0600

    Car D Client

---
+++ src/hooks/useServices.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+
+const useServices = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:4000/services')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
+
+    return services;
+};
+
+export default useServices;
